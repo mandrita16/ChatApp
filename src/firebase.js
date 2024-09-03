b8694b6ba8803bb83c8b56183b6aabd1016c122b
Tridib Dawn <tridibdawn1@gmail.com>
2024-09-03 23:48:58 +0530

    firebase credentials added

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyA1t24YVZR9pJ4T-_WK7dRlwgsJT-XXR70",
+  authDomain: "fir-react-chat-app-d4a9d.firebaseapp.com",
+  projectId: "fir-react-chat-app-d4a9d",
+  storageBucket: "fir-react-chat-app-d4a9d.appspot.com",
+  messagingSenderId: "969878242719",
+  appId: "1:969878242719:web:52d9972c2b3f3742a529aa",
+  measurementId: "G-S0LKD1201X",
 };
 
 // Initialize Firebase
